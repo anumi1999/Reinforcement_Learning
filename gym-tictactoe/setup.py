99a82eb8528b6a208d5875072d5485e848c3e01f
Muskan Jain <muskanjain@iisc.ac.in>
2025-10-26 00:47:51 +0530

    Revert "init"
    This reverts commit a5e55c4532b982511564019f3ff378cf53b18c9a.

--- gym-tictactoe/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='gym_tictactoe',
-    version='0.0.1',
-    packages=find_packages(),           # Include all packages under gym_tictactoe
-    install_requires=['gymnasium'],     # Use Gymnasium instead of Gym
-)
